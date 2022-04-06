412bba72cbd5023fc33dd56ad17e05443ffb5c5f
Alice Favalessa <101601701+AliceFavalessa@users.noreply.github.com>
2022-04-06 21:40:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int p = 0, s = 1, t = 0, n = 0;
+    cin >> n;
+    for (int i=1; i>0; i++) {
+        t = p + s;
+        if (t <= n) {
+            s = p;
+            p = t;
+            cout << t << endl;
+        } else {
+            break;
+        }
+    }
+    
+return 0;
    return 0;
 }
